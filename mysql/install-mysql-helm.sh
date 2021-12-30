503f851946bca459f0d3799daa7873224a9760a8
hanliang <liang.han@gmail.com>
2021-12-30 15:01:59 +0800

    add helm repo add

--- mysql/install-mysql-helm.sh
+++ mysql/install-mysql-helm.sh
@@ -1,2 +1,4 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
 kubectl create ns dbaas
 helm install demo-mysql-cluster-01 bitnami/mysql --values bitnami-mysql-helm-values.yaml -n dbaas
